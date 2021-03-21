08998f7f92b6e0bd6d0b1566c78f951a2cf75270
Robin Massink <robin.massink@alliander.com>
2021-03-21 23:52:25 +0100

    added comments
    Signed-off-by: Robin Massink <robin.massink@alliander.com>

--- testbench.sh
+++ testbench.sh
@@ -27,10 +27,13 @@ DUT_INTERFACE_DPDK="00:1f.6"   	# dpdk (pci address) interface name
 
 FRAME_SIZES="128,256,512" #64,1024,1280,1518
 
-# use hardware rate limiting instead of software (only supported by 10GBe controllers such as X540)
-USE_RATE_TYPE="sw"  #hw poison
+# settings regarding the type of rate limiting used. Hardware (hw) Constand Bit Rate (cbr), or more bursty (poison)
+# cbr is most predictable and supported. poison might be more realistic, hardware is most reliable
+# NOTE: use of hardware rate limiting instead of software is only supported by 10GBe controllers such as X540
+# the I210 and I350 do not support hardware rate limiting
+USE_RATE_TYPE="cbr"  #options: hw cbr poison
 # maximum number of cores used by each benchmark
-USE_MAX_CORES=3 #TODO implement option in scripts
+USE_MAX_CORES=1 #TODO implement option in scripts, currently 1 core used at maximum
 
 
 TXPORT=0 			# Device to transmit to
@@ -38,33 +41,37 @@ RXPORT=0 			# Device to receive from (set to the same device for using 1 NIC)
 
 # Testing that should be inlcuded in this test run
 #TEST_INTERPACKET="y"
-TEST_INTERPACKET_DURATION=1 	# <single test duration>
+#TEST_INTERPACKET_DURATION=1 	# <single test duration>
 
+# measure maximum troughput
 TEST_THROUGHPUT="y"
 TEST_THROUGHPUT_DURATION=1 	# <single test duration>
 TEST_THROUGHPUT_NUM_ITERATIONS=1 # <amount of test iterations>
 TEST_THROUGHPUT_RTHS=100   	# <throughput rate threshold>
 TEST_THROUGHPUT_MLR=0.1    	# <max throuput loss rate>
 
-#TEST_LATENCY="y"
+# measure link latency at line rate
+TEST_LATENCY="y"
 TEST_LATENCY_DURATION=1 	# <single test duration>
-TEST_LATENCY_RT=1000   		# <throughput rate>
+TEST_LATENCY_RT=1000   		# <throughput rate> TODO: use rate from throughput
 
-#TEST_FRAMELOSS="y"
+# measure frame-loss for each link speed
+TEST_FRAMELOSS="y"
 TEST_FRAMELOSS_DURATION=1 	# <single test duration>
 TEST_FRAMELOSS_GRANULARITY=0.5 	# <1/x steps in test (range: 0-1; where 1 means only 1 step, and 0.1 means 10 steps)>
 
-#TEST_BACKTOBACK="y"
+# measure maximum burst length
+TEST_BACKTOBACK="y"
 TEST_BACKTOBACK_DURATION=1 	# <single test duration>
 TEST_BACKTOBACK_NUM_ITERATIONS=1 # <amount of test iterations>
 TEST_BACKTOBACK_BTHS=5 		# <back-to-back frame threshold>
 
 
 #TEST_IEC61850="y"
-TEST_IEC61850_DURATION=1 	# <single test duration>
+#TEST_IEC61850_DURATION=1 	# <single test duration>
 
 # set to generate report
-#GENERATE_REPORT="y"
+GENERATE_REPORT="y"
 
 ### CONFIG ENDS HERE ###
 
@@ -73,13 +80,13 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-function config_DUT () {
+function config_DUT () { # TODO: TEST IT
     # start program "$1" on DUT
     ssh -o UserKnownHostsFile=/dev/null -i $DUT_SSH_KEY_FILE root@$DUT_HOST $1 &
     return $! # return pid of remote forked process
 }
 
-function unconfig_DUT () {
+function unconfig_DUT () { # TODO: TEST IT
     # kill program PID "$1" on DUT
     ssh -o UserKnownHostsFile=/dev/null -i $DUT_SSH_KEY_FILE root@$DUT_HOST kill $1
     return $? # return result
